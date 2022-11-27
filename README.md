142bba7819fb9b8a99ce73dddc6a7d0093e08147
Pedro Couto <pcriscouto@gmail.com>
2022-11-27 19:59:26 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# ToDoAtarefado - Backend
+
+## Descrição
+
+Está api referece ao backend da aplicação ToDoAtarefado.
+
+## Preview
+![TODOAPP SWAGGER API](./preview-swagger.png)
+
+## Antes da instalação
+
+Subir um docker com banco de dados, o padrão utilizado foi Postgres, porém pode alterar e utilizar o banco relacional que desejar.
+
+## Installation
+
+```bash
+$ npm install
+```
+
+## Principais comandos
+
+```bash
+# development
+$ npm run start
+
+# watch mode
+$ npm run start:dev
+
+# production mode
+$ npm run start:prod
+```
+
+## Test
+
+```bash
+# unit tests
+$ npm run test
+
+# e2e tests
+$ npm run test:e2e
+
+# test coverage
+$ npm run test:cov
+```
---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  const config = new DocumentBuilder()
+    .setTitle('ToDoAtarefado API')
+    .setDescription('Documentação com todos os métodos para manutenção das Tarefas do frontend ToDoAtarefado')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
+  await app.listen(process.env.PORT || 3030);
+}
+bootstrap();
